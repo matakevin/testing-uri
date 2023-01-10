48e94395c406c29cf6bd6f323e2acc299eb46622
diegojuarezbusiness@gmail.com <diegojuarezbusiness@gmail.com>
2023-01-09 20:47:01 -0500

    ..

--- src/test/java/com/chroma/stepDefinitions/StudentAdmissionStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/StudentAdmissionStepDef.java
@@ -1,12 +1,15 @@
 package com.chroma.stepDefinitions;
 
 import com.chroma.pages.DashboardPage;
+import com.chroma.pages.StudentAdmissionPage;
 
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StudentAdmissionStepDef {
 
     DashboardPage dashboardPage = new DashboardPage();
+    StudentAdmissionPage studentAdmissionPage = new StudentAdmissionPage();
 
     @When("navigates to Student Information Module")
     public void navigates_to_Student_Information_Module() {
@@ -15,6 +18,11 @@ public class StudentAdmissionStepDef {
 
     @When("navigates to Student Admission Submodule")
     public void navigates_to_Student_Admission_Submodule() {
-     
+        dashboardPage.studentAdmissionSubmodule.click();
+    }
+
+    @Then("user is directed to Student Admission page with the text {string}")
+    public void user_is_directed_to_Student_Admission_page_with_the_text(String text) {
+
     }
 }
